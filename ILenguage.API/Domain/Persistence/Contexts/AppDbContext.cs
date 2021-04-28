d10b671492015bd7aa352cadfd93966847515aac
Sebastian <sebaas14toulier@gmail.com>
2021-04-28 13:04:11 -0500

    AppDbContext_Suscription

---
+++ ILenguage.API/Domain/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using ILenguage.API.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ILenguage.API.Domain.Persistence.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        public DbSet<Suscription>Suscriptions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Suscription>().ToTable("Suscription");
+            modelBuilder.Entity<Suscription>().HasKey(s => s.Id);
+            modelBuilder.Entity<Suscription>().Property(s => s.Id).IsRequired().ValueGeneratedOnAdd();
+            modelBuilder.Entity<Suscription>().Property(s => s.Name).IsRequired().HasMaxLength(20);
+            
+
+
+        }
+    }
+}
