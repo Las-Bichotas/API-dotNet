027ccaaca71df1769bfe4e6979245d4ed5f74145
Sebastian <sebaas14toulier@gmail.com>
2021-04-28 12:03:14 -0500

    SuscriptionModel

---
+++ ILenguage.API/Domain/Models/Suscription.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ILenguage.API.Domain.Models
+{
+    public class Suscription
+    {
+        public int Id { get; set; }
+        public float Price { get; set; }
+        public int Duration { get; set; }
+        public string Name { get; set; }
+        
+        //TODO: 
+        //! public List<UserSuscription> UserSuscriptions { get; set; }
+    }
+}
