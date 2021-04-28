082ac85f73adf9b7b5b35343a31b5855810d9d0b
XantX <hp_xantx@hotmail.com>
2021-04-28 03:50:13 -0500

    Feat: add userService and BaseResponse se agrego el base response y los servicios inciales para el usuario y la asignacion de usuarios entre usuarios

---
+++ ILenguage.API/Domain/Services/IRelatedUser.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ILenguage.API.Domain.Models;
+using ILenguage.API.Domain.Services.Communications;
+
+namespace ILenguage.API.Domain.Services
+{
+    public interface IRelatedUser
+    {
+        Task<IEnumerable<RelatedUser>> ListAsync();
+        Task<IEnumerable<RelatedUser>> ListByIdAsync(int userId);
+        Task<RelatedUserResponse> AssignUserAsync(int userOneId, int userTwoId);
+        Task<RelatedUserResponse> UnassignUserAsync(int userOneId, int userTwoId);
+    }
+}
--- ILenguage.API/Resources/UserResource.cs
+++ ILenguage.API/Resources/UserResource.cs
@@ -9,5 +9,7 @@ namespace ILenguage.API.Resources
         public string Email { get; set; }
         public string Password { get; set; }
         public string Description { get; set; }
+        //TODO:RoleResoure add
+        //public RoleResource Role {get; set;}
     }
 }
