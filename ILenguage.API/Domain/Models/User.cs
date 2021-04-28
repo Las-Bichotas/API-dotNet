66ce6b56ff740e6b92d52d9c31fe8fa9d4098005
XantX <hp_xantx@hotmail.com>
2021-04-28 02:38:45 -0500

    feat: Add class Related User se agrego la clase related user para poder representar la relacion muchos a muchos entre los usuarios de la aplicacion

--- ILenguage.API/Domain/Models/User.cs
+++ ILenguage.API/Domain/Models/User.cs
@@ -10,13 +10,15 @@ namespace ILenguage.API.Domain.Models
         public string email { get; set; }
         public string password { get; set; }
         public string description { get; set; }
-        public int roleId { get; set; }
+        //TODO: falta como representar la foto de perfil
+        // public byte profilePhoto {get; set;}
         //TODO: Esperando a modelo Role
+        public int roleId { get; set; }
         public Role role { get; set; }
         //TODO: PayMethos falta implementar
-        public List<PayMethods> payMethods { get; set; } = new List<PayMethods>();
-
-        public List<User> relatedUsers { get; set; } = new List<User>();
+        public List<PayMethod> PayMethods { get; set; } = new List<PayMethod>();
+        public List<Session> relatedSessions { get; set; } = new List<Session>();
+        public List<RelatedUser> relatedUsers { get; set; }
         public List<EInterestBank> relatedInterest { get; set; } = new List<EInterestBank>();
         public List<ELenguageBank> relatedLenguageInterest { get; set; } = new List<ELenguageBank>();
 
