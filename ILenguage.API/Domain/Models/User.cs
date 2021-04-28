2028f8ca616ecd979426a6aacf3bc9c6121b8060
XantX <hp_xantx@hotmail.com>
2021-04-28 02:02:39 -0500

    feat: add list lenguage and pay methos list Al usuario se le agregaron las listas para sus metodos de pago y sus lenguajes de interes TODO: falta por implementar el pay methods como modelo o enum aun esta por verse

--- ILenguage.API/Domain/Models/User.cs
+++ ILenguage.API/Domain/Models/User.cs
@@ -8,17 +8,17 @@ namespace ILenguage.API.Domain.Models
         public string name { get; set; }
         public string lastName { get; set; }
         public string email { get; set; }
-
         public string password { get; set; }
-
+        public string description { get; set; }
         public int roleId { get; set; }
         //TODO: Esperando a modelo Role
         public Role role { get; set; }
+        //TODO: PayMethos falta implementar
+        public List<PayMethods> payMethods { get; set; } = new List<PayMethods>();
 
         public List<User> relatedUsers { get; set; } = new List<User>();
-
-        //TODO: Lista de intereses
-        public List<ELenguageBank> relatedInterest { get; set; } = new List<ELenguageBank>();
+        public List<EInterestBank> relatedInterest { get; set; } = new List<EInterestBank>();
+        public List<ELenguageBank> relatedLenguageInterest { get; set; } = new List<ELenguageBank>();
 
     }
 }
