d3de4924ded017459e9e40b0dad3880cc3841a7d
Sebastian <sebaas14toulier@gmail.com>
2021-04-28 11:48:18 -0500

    stringExtensions

---
+++ ILenguage.API/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Xml.Schema;
+
+namespace ILenguage.API.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToSnakeCase(this string str)
+        {
+            return string.Concat(
+                str.Select((x, i) => i > 0 && char.IsUpper(x)
+                    ? "_" + x.ToString()
+                    : x.ToString())).ToLower();
+        }
+    }
+}
