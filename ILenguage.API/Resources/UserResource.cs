f7273d9a713e8c0f38751e8e4b56787db1d67708
XantX <hp_xantx@hotmail.com>
2021-04-28 03:03:32 -0500

    feat: add resource user se agrego el recource del user tanto para el imput como para el output TODO: corregir los comentarios en el modelo User

---
+++ ILenguage.API/Resources/UserResource.cs
@@ -0,0 +1,13 @@
+namespace ILenguage.API.Resources
+{
+    public class UserResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+    }
+}
