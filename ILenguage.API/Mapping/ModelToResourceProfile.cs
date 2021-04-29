b3c10124262f6b760b80aa5cfeb4d7188abe5777
Sebastian <sebaas14toulier@gmail.com>
2021-04-29 03:02:30 -0500

    Mapping

---
+++ ILenguage.API/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ILenguage.API.Domain.Models;
+using ILenguage.API.Resources;
+
+namespace ILenguage.API.Mapping
+{
+    public class ModelToResourceProfile : Profile
+    {
+        public ModelToResourceProfile()
+        {
+            CreateMap<Suscription, SuscriptionResource>();
+        }
+    }
+}
