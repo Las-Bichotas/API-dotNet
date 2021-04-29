46cd137ce9e90f51056befe6d84b6424e6930d35
Sebastian <sebaas14toulier@gmail.com>
2021-04-29 01:31:57 -0500

    PaymentMethodRepoInterface

---
+++ ILenguage.API/Domain/Models/PaymentMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic;
+
+namespace ILenguage.API.Domain.Models
+{
+    public class PaymentMethod
+    {
+        public ulong CardNumber { get; set; }
+        public string OwnerName { get; set; }
+        public string Year { get; set; }
+        [Range(1,12,ErrorMessage = "Month must be between 1 to 12")]
+        public uint month { get; set; }
+        public uint CvcCode { get; set; }
+        public EPaymentNetwork PaymentNetwork { get; set; }
+        //TODO: 
+        //TODO: public User User { get; set; }
+    }
+}
--- ILenguage.API/Domain/Persistence/Contexts/AppDbContext.cs
+++ ILenguage.API/Domain/Persistence/Contexts/AppDbContext.cs
@@ -12,15 +12,31 @@ namespace ILenguage.API.Domain.Persistence.Contexts
         }
         
         public DbSet<Suscription>Suscriptions { get; set; }
+        public DbSet<PaymentMethod>PaymentMethods { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            
+            //!Suscription
             modelBuilder.Entity<Suscription>().ToTable("Suscription");
             modelBuilder.Entity<Suscription>().HasKey(s => s.Id);
             modelBuilder.Entity<Suscription>().Property(s => s.Id).IsRequired().ValueGeneratedOnAdd();
             modelBuilder.Entity<Suscription>().Property(s => s.Name).IsRequired().HasMaxLength(20);
             
+            //!PaymentMethod
+            modelBuilder.Entity<PaymentMethod>().ToTable("PaymentMethod");
+            modelBuilder.Entity<PaymentMethod>().HasKey(pm => pm.CardNumber);
+            modelBuilder.Entity<PaymentMethod>().Property(pm => pm.CardNumber).IsRequired();
+            modelBuilder.Entity<PaymentMethod>().Property(pm => pm.CvcCode).IsRequired();
+            modelBuilder.Entity<PaymentMethod>().Property(pm => pm.OwnerName).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<PaymentMethod>().Property(pm => pm.Year).IsRequired();
+            modelBuilder.Entity<PaymentMethod>().Property(pm => pm.month).IsRequired();
+            modelBuilder.Entity<PaymentMethod>().Property(pm => pm.PaymentNetwork).IsRequired();
+            
+
+
+
 
 
         }
