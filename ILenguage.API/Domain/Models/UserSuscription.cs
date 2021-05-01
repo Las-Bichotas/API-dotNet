a4bb411ee4224825f88aa09bcb3ed11b1317b81c
Sebastian <sebaas14toulier@gmail.com>
2021-05-01 09:47:37 -0500

    UserSuscriptionModel

---
+++ ILenguage.API/Domain/Models/UserSuscription.cs
@@ -0,0 +1,15 @@
+﻿namespace ILenguage.API.Domain.Models
+{
+    public class UserSuscription
+    {
+        public int SuscriptionId { get; set; }
+        public Suscription Suscription { get; set; }
+        
+        public int UserId { get; set; }
+       //! public User User { get; set; }
+       
+       public ulong CardNumber { get; set; }
+       public PaymentMethod PaymentMethod { get; set; }
+        
+    }
+}
