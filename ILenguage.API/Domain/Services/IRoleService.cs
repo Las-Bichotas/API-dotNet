3cab4ead3cc13af60b59514e67c4a0f3f2c613ca
Sebastian Trujillo <ippo_sebas@hotmail.com>
2021-05-03 18:23:16 -0500

    add IRoleService in Services

---
+++ ILenguage.API/Domain/Services/IRoleService.cs
@@ -0,0 +1,19 @@
+﻿using ILanguage.API.Domain.Models;
+using ILanguage.API.Domain.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ILanguage.API.Domain.Services
+{
+    interface IRoleService
+    {
+        Task<IEnumerable<Role>> ListAsync();
+        Task<RoleResponse> GetByIdAsync(int id);
+        Task<RoleResponse> SaveAsync(Role role);
+        Task<RoleResponse> UpdateAsync(int id, Role role);
+        Task<RoleResponse> DeleteAsync(int id);
+
+    }
+}
