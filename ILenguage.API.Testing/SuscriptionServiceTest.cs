8c6b4e0b7e981c0d9a586438b11f4901d95cf2c2
Sebastian <sebaas14toulier@gmail.com>
2021-05-02 20:12:23 -0500

    bfr tst

---
+++ ILenguage.API.Testing/SuscriptionServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+namespace ILenguage.API.Testing
+{
+    public class SuscriptionServiceTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public async Task GetAllAyncWhenNoSuscriptionsReturnsEmptyCollection()
+        {
+            
+        }
+        
+        
+        
+    }
+}
