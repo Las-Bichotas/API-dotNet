a146d47d2a14b997992da5f8d5807659f925b8cb
Sebastian Trujillo <ippo_sebas@hotmail.com>
2021-05-03 18:27:25 -0500

    Add RoleResource and SaveRoleResource in Resources

---
+++ ILenguage.API/Resources/RoleResource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ILanguage.API.Resources
+{
+    public class RoleResource
+    { 
+        //sirve para exponer informacion hacia el exterior
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ILenguage.API/Resources/SaveRoleResource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;//anotaciones y entradas de datos
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ILanguage.API.Resources
+{
+    public class SaveRoleResource
+    {
+        //sirve para guardar la informacion
+        [Required]//condiciones
+        [MaxLength(30)]//condiciones
+        //solo guardamos el nombre debido a que el ID se autogenera
+        public string Name { get; set; }
+    }
+}
