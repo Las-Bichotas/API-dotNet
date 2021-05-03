dc52d93402756a9d46cc85b03eef390cf04cec22
Sebastian Trujillo <ippo_sebas@hotmail.com>
2021-05-03 18:22:06 -0500

    add IRoleRepository in Repositories

---
+++ ILenguage.API/Domain/Persistence/Repositories/IRoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using ILanguage.API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ILanguage.API.Domain.Repositories
+{
+    public interface IRoleRepository
+    {
+        Task<IEnumerable<Role>> ListAsync();
+        Task AddAsync(Role role);
+        Task<Role> FindById(int id);
+        void Update(Role role);
+        void Remove(Role role);
+    }
+}
