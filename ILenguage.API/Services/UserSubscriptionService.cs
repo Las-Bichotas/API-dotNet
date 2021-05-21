974a1009f326f2a93090fbc6b6eefb7ab3c1d681
Sebastian Toulier <65046165+SebTou08@users.noreply.github.com>
2021-05-21 18:20:29 -0500

    feat: implement User Subscription service

---
+++ ILenguage.API/Services/UserSubscriptionService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ILenguage.API.Domain.Models;
+using ILenguage.API.Domain.Persistence.Repositories;
+using ILenguage.API.Domain.Services;
+using ILenguage.API.Domain.Services.Communications;
+
+namespace ILenguage.API.Services
+{
+    public class UserSubscriptionService : IUserSubscriptionService
+    {
+        private readonly IUserSubscriptionRepository _userSubscriptionRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserSubscriptionService(IUserSubscriptionRepository userSubscriptionRepository, IUnitOfWork unitOfWork)
+        {
+            _userSubscriptionRepository = userSubscriptionRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<UserSubscription>> ListAsync()
+        {
+            return await _userSubscriptionRepository.ListAsync();
+        }
+
+        public async Task<IEnumerable<UserSubscription>> ListByUserIdAsync(int userId)
+        {
+            return await _userSubscriptionRepository.ListByUserId(userId);
+        }
+
+        public async Task<IEnumerable<UserSubscription>> ListBySubscriptionId(int suscriptionId)
+        {
+            return await _userSubscriptionRepository.ListBySubscriptionId(suscriptionId);
+        }
+
+        public async Task<UserSubscriptionResponse> AssingUserSubscriptionAsync(int userId, int subscriptionId)
+        {
+            try
+            {
+                await _userSubscriptionRepository.AssingUserSubscription(userId, subscriptionId);
+                await _unitOfWork.CompleteAsync();
+                UserSubscription userSubscription =
+                    await _userSubscriptionRepository.FindBySubscriptionIdAndUserId(subscriptionId, userId);
+                return new UserSubscriptionResponse(userSubscription);
+            }
+            catch (Exception e)
+            {
+                return new UserSubscriptionResponse(
+                    $"An error ocurred while assingin User to Subscription: {e.Message} ");
+            }
+        }
+
+        public async Task<UserSubscriptionResponse> UnassingUserSubscriptionAsync(int userId, int subscriptionId)
+        {
+            try
+            {
+                UserSubscription userSubscription =
+                    await _userSubscriptionRepository.FindBySubscriptionIdAndUserId(subscriptionId, userId);
+                _userSubscriptionRepository.Remove(userSubscription);
+                await _unitOfWork.CompleteAsync();
+                return new UserSubscriptionResponse(userSubscription);
+            }
+            catch (Exception e)
+            {
+                return new UserSubscriptionResponse(
+                    $"An error ocurred while unassinging User From Subscription: {e.Message}");
+            }
+        }
+    }
+}
