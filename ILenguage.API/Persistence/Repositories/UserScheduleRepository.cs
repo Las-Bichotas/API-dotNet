b782891a0c477922bc3f7fc554223dfd32ec090a
David-lp-ops <54964742+David-lp-ops@users.noreply.github.com>
2021-05-23 13:54:07 -0500

    fix:Create UserScheduleRepository

---
+++ ILenguage.API/Persistence/Repositories/UserScheduleRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ILenguage.API.Domain.Models;
+using ILenguage.API.Domain.Persistence.Contexts;
+using ILenguage.API.Domain.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ILenguage.API.Persistence.Repositories
+{
+    public class UserScheduleRepository : BaseRepository, IUserScheduleRepository
+    {
+        public UserScheduleRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        
+
+       
+        
+    }
+}
